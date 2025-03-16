a181ba6ad78d68fadc821cb3c3ebe0236d6e58bf
James Inman <james@jamesinman.co.uk>
2025-03-16 19:50:13 +0000

    Namespace migrations correctly
    Without this we can't run the tests in isolation, and possibly installing migrations into a new app wouldn't work either

--- db/migrate/20250121012334_enable_hstore.rb
+++ db/migrate/20250121012334_enable_hstore.rb
@@ -1,9 +1,5 @@
-module Panda
-  module Core
-    class EnableHstore < ActiveRecord::Migration[8.0]
-      def change
-        enable_extension :hstore
-      end
-    end
+class EnableHstore < ActiveRecord::Migration[8.0]
+  def change
+    enable_extension :hstore
   end
 end
