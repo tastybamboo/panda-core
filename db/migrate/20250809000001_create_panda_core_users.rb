65524dde33930b8330f1a37a2350a23d7c7726f1
James Inman <james@jamesinman.co.uk>
2025-08-11 19:40:47 +0100

    Fix User migration and schema to match model attributes
    - Update migration to use firstname, lastname, and admin attributes
    - Rename migration file to correct timestamp (20250809000001)
    - Update schema.rb with correct column names
    - Fixes CI failures due to attribute mismatches

--- db/migrate/20250809000001_create_panda_core_users.rb
+++ db/migrate/20250809000001_create_panda_core_users.rb
@@ -3,10 +3,11 @@
 class CreatePandaCoreUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :panda_core_users, id: :uuid do |t|
-      t.string :name, null: false
+      t.string :firstname, null: false
+      t.string :lastname, null: false
       t.string :email, null: false
       t.string :image_url
-      t.boolean :is_admin, default: false, null: false
+      t.boolean :admin, default: false, null: false
       t.timestamps
     end
 
