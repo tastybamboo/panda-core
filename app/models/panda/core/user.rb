5461c7224b34587147d96a9236c362a8c4151446
James Inman <james@jamesinman.co.uk>
2025-10-28 12:07:25 +0000

    fix: Use is_admin column instead of admin in User model

--- app/models/panda/core/user.rb
+++ app/models/panda/core/user.rb
@@ -10,31 +10,22 @@ module Panda
       before_save :downcase_email
 
       # Scopes
-      scope :admin, -> { where(admin: true) }
+      scope :admin, -> { where(is_admin: true) }
 
       def self.find_or_create_from_auth_hash(auth_hash)
         user = find_by(email: auth_hash.info.email.downcase)
         return user if user
 
-        # Parse name into first and last
-        full_name = auth_hash.info.name || "Unknown User"
-        name_parts = full_name.split(" ", 2)
-
         create!(
           email: auth_hash.info.email.downcase,
-          firstname: name_parts[0] || "Unknown",
-          lastname: name_parts[1] || "",
+          name: auth_hash.info.name || "Unknown User",
           image_url: auth_hash.info.image,
-          admin: User.count.zero? # First user is admin
+          is_admin: User.count.zero? # First user is admin
         )
       end
 
       def admin?
-        admin == true
-      end
-
-      def name
-        "#{firstname} #{lastname}".strip
+        is_admin?
       end
 
       def active_for_authentication?
