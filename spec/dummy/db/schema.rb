e0222f668ed82d99c629d579e9350e77ab4d5c08
James Inman <james@jamesinman.co.uk>
2025-11-26 00:07:21 +0000

    Updating DB schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_21_005640) do
+ActiveRecord::Schema[8.1].define(version: 20250809000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -63,11 +63,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_21_005640) do
   end
 
   create_table "panda_core_users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.string "current_theme"
     t.string "email", null: false
     t.string "image_url"
-    t.boolean "admin", default: false, null: false
     t.string "name"
     t.string "oauth_avatar_url"
     t.datetime "updated_at", null: false
