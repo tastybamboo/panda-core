607ad5b32e016a091369feb6c46e70974f0209b2
James Inman <james@jamesinman.co.uk>
2025-12-13 21:19:44 +0000

    Fix autoloading issue for AdminController in production
    Add require_dependency to eager load BaseController before referencing it
    to avoid NameError during asset precompilation in production mode.

--- lib/panda/core/engine/admin_controller_config.rb
+++ lib/panda/core/engine/admin_controller_config.rb
@@ -12,6 +12,8 @@ module Panda
           # This allows other gems to inherit from Panda::Core::AdminController
           initializer "panda_core.admin_controller_alias", after: :load_config_initializers do
             ActiveSupport.on_load(:action_controller_base) do
+              # Eager load the BaseController to avoid autoload issues in production
+              require_dependency "panda/core/admin/base_controller" if defined?(Rails) && Rails.env.production?
               Panda::Core.const_set(:AdminController, Panda::Core::Admin::BaseController) unless Panda::Core.const_defined?(:AdminController)
             end
           end
